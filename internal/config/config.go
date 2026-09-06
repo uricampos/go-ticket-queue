36e2bdad2e92a817dd9ea3e4157af9a91f194512
Uriel Campos <urieldecampos@gmail.com>
2026-09-06 16:05:02 -0300

    feat: create internal config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,34 @@
 package config
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Env        string
+	DBPort     string
+	DBName     string
+	DBUser     string
+	DBPassword string
+	ServerPort string
+}
+
+func Load() (*Config, error) {
+
+	err := godotenv.Load()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &Config{
+		Env:        os.Getenv("ENV"),
+		DBPort:     os.Getenv("DB_PORT"),
+		DBName:     os.Getenv("DB_NAME"),
+		DBUser:     os.Getenv("DB_USER"),
+		DBPassword: os.Getenv("DB_PASSWORD"),
+		ServerPort: os.Getenv("SERVER_PORT"),
+	}, nil
+}
