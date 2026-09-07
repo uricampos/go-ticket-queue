d6397db3e07a86cc0f88eab4477a3cf443341704
Uriel Campos <urieldecampos@gmail.com>
2026-09-07 16:06:37 -0300

    feat: create users service

--- internal/domain/users/users_service.go
+++ internal/domain/users/users_service.go
@@ -1,1 +1,59 @@
 package users
+
+import (
+	"context"
+	"errors"
+
+	"github.com/google/uuid"
+)
+
+type UserService struct {
+	repo UserRepository
+}
+
+func NewUserService(repo UserRepository) *UserService {
+	return &UserService{
+		repo: repo,
+	}
+}
+
+func (s *UserService) CreateUser(ctx context.Context, username string) (*User, error) {
+
+	user, err := s.repo.GetUserByUsername(ctx, username)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if user != nil {
+		return nil, errors.New("username already taken")
+	}
+
+	user, err = s.repo.CreateUser(ctx, username)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (s *UserService) GetUserByUsername(ctx context.Context, username string) (*User, error) {
+	user, err := s.repo.GetUserByUsername(ctx, username)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return user, err
+}
+
+func (s *UserService) GetUserByID(ctx context.Context, id uuid.UUID) (*User, error) {
+	user, err := s.repo.GetUserByID(ctx, id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
