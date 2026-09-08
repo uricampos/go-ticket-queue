33ddf4ca5262c7b17c10290cfab70250a194696c
Uriel Campos <urieldecampos@gmail.com>
2026-09-08 00:34:43 -0300

    feat: setup users dependencies and routes in main

--- cmd/main.go
+++ cmd/main.go
@@ -3,8 +3,13 @@ package main
 import (
 	"log"
 
+	"github.com/gin-gonic/gin"
 	"github.com/uricampos/go-ticket-queue/internal/config"
+	usersDomain "github.com/uricampos/go-ticket-queue/internal/domain/users"
+	"github.com/uricampos/go-ticket-queue/internal/infra/http/routes"
+	usersInfra "github.com/uricampos/go-ticket-queue/internal/infra/http/users"
 	"github.com/uricampos/go-ticket-queue/internal/infra/postgres"
+	usersRepo "github.com/uricampos/go-ticket-queue/internal/infra/postgres/users"
 )
 
 func main() {
@@ -26,6 +31,15 @@ func main() {
 	defer db.Close()
 
 	// setup routes
+	router := gin.Default()
+
+	//users
+	userRepo := usersRepo.NewUserRepository(db)
+	userService := usersDomain.NewUserService(userRepo)
+	userHandler := usersInfra.NewUserHandler(*userService)
+
+	routes.SetupUsersRoutes(router, userHandler)
 
 	// server listen
+	router.Run(":" + cfg.ServerPort)
 }
